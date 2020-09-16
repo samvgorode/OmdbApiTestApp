5d476b0fab713933ae61144a02c6897ce3367ddf
Eugene P <yevhenp@truckersreport.com>
2020-09-16 16:42:14 +0300

    get rid of compose

--- app/src/main/java/com/example/omdbapitestapp/system/MainActivity.kt
+++ app/src/main/java/com/example/omdbapitestapp/system/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.omdbapitestapp.system
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.omdbapitestapp.R
 import com.example.omdbapitestapp.presentation.MainViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -14,6 +14,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.main_activity)
     }
 }
